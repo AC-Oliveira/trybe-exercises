81548dc26af366a31af08f7268e0e0ae0e2725dd
AC-Oliveira <allancardoso21@gmail.com>
2021-06-24 11:22:14 -0300

    Bonus Resolvido.

--- exercises/higher-order-functions-do-javascript-es6_1/Bonus/gameActions.js
+++ exercises/higher-order-functions-do-javascript-es6_1/Bonus/gameActions.js
@@ -2,7 +2,7 @@ let mageMana = 125;
 let mageManaDamage = function() {
   const obj = {mageDamage: () => 44 + Math.ceil(Math.random()*46) , mana: () => {
     if ( mageMana < 15) {
-      console.log('Não possui mana suficiente"')
+      console.log('Mana insuficiente o ataque do Mago não pode ser realizado.')
       return mageMana;
     }
     mageMana -= 15;
@@ -35,3 +35,50 @@ const dragon = {
 
 const battleMembers = { mage, warrior, dragon };
 
+const gameActions = {
+  warriorAtack: () => {
+    if (warrior.healthPoints <= 0) {
+      return console.log('Guerreiro foi derrotado e não poderá atacar!');
+    }
+    dragon.healthPoints = dragon.healthPoints - warrior.damage;
+    if (dragon.healthPoints <= 0 ) {
+      return console.log('O Dragão morreu!');
+    }
+    return console.log(`Guereiro ataca causa ${warrior.damage} de dano ao Dragão possui ${dragon.healthPoints} pontos de vida restantes!`);
+  },
+  mageAtack: () => {
+    if (mage.healthPoints <= 0) {
+      return console.log('Mago foi derrotado e não poderá atacar!');
+    }
+    dragon.healthPoints = dragon.healthPoints - mage.damage;
+    mage.mana();
+    if (dragon.healthPoints <= 0 ) {
+      return console.log('O Dragão morreu!');
+    }
+    return console.log(`Mago ataca causa ${mage.damage} de dano ao Dragão. Dragão possui ${dragon.healthPoints} pontos de vida restantes! Mana restante ${mageMana}`);
+  },
+  dragonAtack: () => {
+    if (dragon.healthPoints <= 0) {
+      return console.log('Dragão foi derrotado e não poderá atacar!')
+    }
+    warrior.healthPoints = warrior.healthPoints - dragon.damage;
+    mage.healthPoints = mage.healthPoints - dragon.damage;
+    if(mage.healthPoints <= 0 && warrior.healthPoints <= 0) {
+      return console.log('O Dragão venceu!')
+    }
+    if(mage.healthPoints <= 0) {
+      console.log('O mago foi derrotado!');
+    }
+    return console.log(`Dragão ataca causa ${dragon.damage} de dano ao Mago e Guerreito. Mago possui ${mage.healthPoints} pontos de vida restantes e o Guerreiro possui ${warrior.healthPoints} pontos de vida restantes!`);
+  }
+};
+
+gameActions.warriorAtack()
+gameActions.dragonAtack()
+gameActions.mageAtack()
+gameActions.warriorAtack()
+gameActions.dragonAtack()
+gameActions.mageAtack()
+gameActions.warriorAtack()
+gameActions.dragonAtack()
+gameActions.mageAtack()
